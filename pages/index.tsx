094bf32e7c3c33f99a6a8046007b0e9eb074d954
Chris Williams <chris.r.williams@icloud.com>
2023-02-24 23:32:07 -0500

    add og tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,56 @@
 import { Scene } from "../src/Scene";
 import { Link, Typography } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import Head from "next/head";
 
 export default function Home() {
 	return (
 		<>
+			<Head>
+				{/* Primary Meta Tags */}
+				<title>
+					Arsenal Football Club Supporters Group in Miami, FL - Miami Gooners
+				</title>
+				<meta
+					name='title'
+					content='Arsenal Football Club Supporters Group in Miami, FL - Miami Gooners'
+				/>
+				<meta
+					name='description'
+					content='Miami Gooners, the Rebel Arsenal Football Club Supporters Group in Miami, Florida.  Follow us on instagram @miamigooners.'
+				/>
+
+				{/* Open Graph / Facebook */}
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://miamigooners.com/' />
+				<meta
+					property='og:title'
+					content='Arsenal Football Club Supporters Group in Miami, FL - Miami Gooners'
+				/>
+				<meta
+					property='og:description'
+					content='Miami Gooners, the Rebel Arsenal Football Club Supporters Group in Miami, Florida.  Follow us on instagram @miamigooners.'
+				/>
+				<meta
+					property='og:image'
+					content='https://miamigooners.com/og-image.png'
+				/>
+
+				{/* Twitter */}
+				<meta property='twitter:card' content='summary_large_image' />
+				<meta property='twitter:url' content='https://miamigooners.com/' />
+				<meta
+					property='twitter:title'
+					content='Arsenal Football Club Supporters Group in Miami, FL - Miami Gooners'
+				/>
+				<meta
+					property='twitter:description'
+					content='Miami Gooners, the Rebel Arsenal Football Club Supporters Group in Miami, Florida.  Follow us on instagram @miamigooners.'
+				/>
+				<meta
+					property='twitter:image'
+					content='https://miamigooners.com/og-image.png'></meta>
+			</Head>
 			<div
 				style={{
 					position: "absolute",
