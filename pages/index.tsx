18799a06bc07a8aa91a867e202b7874d02192964
Chris Williams <chris.r.williams@icloud.com>
2023-02-24 22:44:33 -0500

    update links, add background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-import React, { useRef, useState, useEffect } from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import { Scene } from "../src/Scene";
+import { Link } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 export default function Home() {
 	return (
-		<Container maxWidth={false} disableGutters>
-			<Box sx={{ width: "100vw", height: "100vh" }}></Box>
-		</Container>
+		<>
+			<div
+				style={{
+					position: "absolute",
+					zIndex: "-1",
+					inset: 0,
+					width: "100vw",
+					height: "100vh",
+					background: "url(/background.jpeg) no-repeat center center fixed",
+					backgroundSize: "cover",
+				}}></div>
+			<Scene />
+			<div style={{ position: "absolute", top: "2rem", right: "2rem" }}>
+				<Link href='https://www.instagram.com/miamigooners/'>
+					<InstagramIcon color='primary' fontSize='large' />
+				</Link>
+			</div>
+		</>
 	);
 }
---
+++ src/Scene.tsx
@@ -0,0 +1,38 @@
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Environment, OrbitControls } from "@react-three/drei";
+
+const Model = () => {
+	const gltf = useLoader(GLTFLoader, "/gooners-logo.glb");
+	const modelRef = useRef();
+
+	useFrame(() => {
+		modelRef.current.rotation.z += -0.01;
+	});
+
+	return (
+		<>
+			<primitive
+				object={gltf.scene}
+				scale={20}
+				rotation-x={Math.PI * 0.5}
+				ref={modelRef}
+			/>
+		</>
+	);
+};
+
+export const Scene = () => {
+	return (
+		<div style={{ position: "absolute", inset: 0, zIndex: 0 }}>
+			<Canvas>
+				<Suspense fallback={null}>
+					<Model />
+					<OrbitControls />
+					<Environment background={false} preset='sunset' />
+				</Suspense>
+			</Canvas>
+		</div>
+	);
+};
