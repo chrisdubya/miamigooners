5be84d34fde83cc78408e8a683731fc762e00a23
Chris Williams <chris.r.williams@icloud.com>
2023-02-26 11:12:03 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
+import Script from "next/script";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -64,7 +65,22 @@ export default function MyApp(props: MyAppProps) {
 					content='https://miamigooners.com/og-image.png'></meta>
 
 				<meta name='viewport' content='initial-scale=1, width=device-width' />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+						
+							gtag('config', 'G-1ZGEWQ7JLM');
+						`,
+					}}
+				/>
 			</Head>
+			<Script
+				strategy='afterInteractive'
+				src='https://www.googletagmanager.com/gtag/js?id=G-1ZGEWQ7JLM'
+			/>
 			<ThemeProvider theme={theme}>
 				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
