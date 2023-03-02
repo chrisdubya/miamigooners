ed51f66295e7dce6078b10a5a540d80a1f88beb5
Chris Williams <chris.r.williams@icloud.com>
2023-03-02 17:29:03 -0500

    clean up index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,47 @@
 import { Scene } from "../src/Scene";
-import { Link, Typography } from "@mui/material";
+import { Container, Box, Link, Typography } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import { AllEvents } from "../src/AllEvents";
 
 export default function Home() {
 	return (
-		<>
-			<div
-				style={{
-					position: "absolute",
-					zIndex: "-1",
-					inset: 0,
-					width: "100vw",
-					height: "100vh",
-					background: "url(/background.jpeg) no-repeat center center fixed",
-					backgroundSize: "cover",
-				}}></div>
-			<Scene />
-			<div style={{ position: "absolute", top: "2rem", right: "2rem" }}>
-				<Link href='https://www.instagram.com/miamigooners/'>
-					<InstagramIcon color='primary' fontSize='large' />
-				</Link>
-			</div>
-			<div
-				style={{
-					position: "absolute",
-					bottom: "6rem",
-					left: "50%",
-					transform: "translateX(-50%)",
-				}}>
-				<Typography sx={{ fontWeight: "bold" }} variant='body1' color='primary'>
-					coming soon...
-				</Typography>
-			</div>
-		</>
+		<Container>
+			<Box component='div' sx={{ height: "100vh" }}>
+				<div
+					style={{
+						position: "absolute",
+						zIndex: "-1",
+						inset: 0,
+						background: "url(/background.jpeg) no-repeat center center fixed",
+						backgroundSize: "cover",
+					}}></div>
+				<Scene />
+
+				<div style={{ position: "absolute", top: "2rem", right: "2rem" }}>
+					<Link href='https://www.instagram.com/miamigooners/'>
+						<InstagramIcon color='primary' fontSize='large' />
+					</Link>
+				</div>
+
+				<div
+					style={{
+						position: "absolute",
+						bottom: "6rem",
+						left: "50%",
+						transform: "translateX(-50%)",
+					}}>
+					<Typography
+						sx={{ fontWeight: "bold" }}
+						variant='body1'
+						color='primary'>
+						coming soon...
+					</Typography>
+				</div>
+			</Box>
+
+			<Box component='div'>
+				<AllEvents />
+			</Box>
+		</Container>
 	);
 }
