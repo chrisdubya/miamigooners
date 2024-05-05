90e3b67a2c976dbb468382941d46204bc7373e8a
Chris Williams <chriscancodethat@gmail.com>
2024-05-05 12:13:11 -0400

    add new assets add memberName and membershipNumber form

--- pages/api/passes/index.ts
+++ pages/api/passes/index.ts
@@ -2,24 +2,60 @@ import { Template } from "@walletpass/pass-js";
 import fs from 'fs';
 import path from 'path';
 
+const CURRENT_SEASON = '23/24';
+
+const generatePassImage = async (imageName: string, imageType: 'logo' | 'icon', template: Template, dimensions: '1x' | '2x' | '3x') => {
+    const imagePath = path.join(process.cwd(), 'public/images/wallet-assets', imageName)
+    const imageBuffer = fs.readFileSync(imagePath);
+    await template.images.add(imageType, imageBuffer, dimensions);
+}
+
 export default async function handler(req: any, res: any) {
     if (req.method === 'POST') {
-        const template = new Template("coupon", {
-            passTypeIdentifier: "pass.com.miamigooners.node",
-            teamIdentifier: "S3K8RWSVKB",
-            backgroundColor: "red",
-            sharingProhibited: false,
-            organizationName: "Miami Gooners",
-        });
+        const template = new Template(
+            "generic", 
+            {
+                passTypeIdentifier: "pass.com.miamigooners.node",
+                teamIdentifier: "S3K8RWSVKB",
+                backgroundColor: "black",
+                foregroundColor: "red",
+                labelColor: "white",
+                sharingProhibited: true,
+                organizationName: "Miami Gooners",
+                description: "Miami Gooners Membership Pass",
+                generic: {
+                    headerFields: [
+                        {
+                            key: "season",
+                            label: "Season",
+                            value: CURRENT_SEASON
+                        }
+                    ],
+                    primaryFields: [
+                        {
+                            key: "memberName",
+                            label: "Member",
+                            value: req.body.memberName
+                        }
+                    ],
+                    secondaryFields: [
+                        {
+                            key: "membershipNumber",
+                            label: "Member No.",
+                            value: req.body.membershipNumber
+                        }
+                    ],
+                }
+            }
+        );
 
-        const iconPath = path.join(process.cwd(), 'public', 'favicon-32x32.png');
-        const logoPath = path.join(process.cwd(), 'public', 'favicon-32x32.png');
+        await generatePassImage('gooners-wallet-logo.png', 'icon', template, '1x');
+        await generatePassImage('gooners-wallet-logo@2x.png', 'icon', template, '2x');
+        await generatePassImage('gooners-wallet-logo@3x.png', 'icon', template, '3x');
 
-        const iconBuffer = fs.readFileSync(iconPath);
-        const logoBuffer = fs.readFileSync(logoPath);
-
-        await template.images.add("icon", iconBuffer);
-        await template.images.add("logo", logoBuffer);
+        await generatePassImage('gooners-wallet-logo.png', 'logo', template, '1x');
+        await generatePassImage('gooners-wallet-logo@2x.png', 'logo', template, '2x');
+        await generatePassImage('gooners-wallet-logo@3x.png', 'logo', template, '3x');
 
         const pemPath = path.join(process.cwd(), 'private/passes', 'signerCert.pem');
 
@@ -27,7 +63,6 @@ export default async function handler(req: any, res: any) {
         
         const pass = template.createPass({
             serialNumber: "123456",
-            description: "20% off"
         });
 
         await pass.asBuffer().then((buffer) => {
--- pages/pass/index.tsx
+++ pages/pass/index.tsx
@@ -1,44 +1,92 @@
 import axios from "axios";
 import { useState } from "react";
-import goonerIcon from "/public/gooners-logo.svg";
+import goonerIcon from "/public/images/logos/gooners-logo.svg";
 import Image from "next/image";
 
 export default function Pass() {
     const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [memberName, setMemberName] = useState<string>('');
+    const [membershipNumber, setMembershipNumber] = useState<string>('');
 
-    const handleCreatePass = async () => {
+    const handleCreatePass = async (e: any) => {
+        e.preventDefault();
+
         try {
-            const response = await axios.post('/api/passes', {}, {
+            setLoading(true);
+
+            const response = await axios.post('/api/passes', {
+                memberName: memberName,
+                membershipNumber: membershipNumber,
+            }, {
                 responseType: 'blob'
             });
+
+            if (response.status !== 200) {
+                throw new Error('Error creating pass');
+            }
             
             const url = window.URL.createObjectURL(new Blob([response.data]));
             setDownloadUrl(url);
+
+            setMemberName('');
+            setMembershipNumber('');
         } catch (error) {
-            console.error('Error downloading pass:', error);
+            console.error('Pass Error:', error);
+        } finally {
+            setLoading(false);
         }
     }
 
     return (
-        <div className="mx-auto max-w-screen-2xl flex justify-center items-center h-screen flex-col gap-12">
-            <Image src={goonerIcon} alt="Gooner Icon" width={300} height={300} />
+        <div className="mx-auto max-w-screen-2xl flex items-center h-[calc(100vh-2rem)] flex-col gap-12 mt-8">
+            <Image src={goonerIcon} alt="Gooner Icon" width={250} height={250} />
 
-            <div className="flex flex-col gap-4 justify-center items-center">
-                <h1 className="text-center text-xl md:text-3xl">Create A Membership Pass</h1>
-                
-                {!downloadUrl ? (
-                    <button 
-                        className="uppercase font-bold flex justify-center items-center border border-white rounded-3xl p-2 md:p-4 hover:border-gooner-red hover:text-gooner-red min-w-[150px] md:min-w-[200px] transition-colors duration-500" 
-                        type="button" 
-                        onClick={handleCreatePass}
-                    >
-                        Create
-                    </button>
-                ) : (
+            <div className="flex flex-col gap-4 justify-center items-center max-w-80">
+                <h1 className="text-center text-xl md:text-3xl">Create Membership Pass</h1>
+
+                <form className="w-full" onSubmit={handleCreatePass}>
+                    <div className="flex flex-col gap-4">
+                        <input
+                            required
+                            type="text" 
+                            id="memberName" 
+                            name="memberName"
+                            value={memberName}
+                            onChange={e => setMemberName(e.target.value)}
+                            className="w-full border border-white rounded-xl p-2 md:p-4 text-black" 
+                            placeholder="Member Name" 
+                        />
+
+                        <input 
+                            required
+                            type="text" 
+                            id="membershipNumber" 
+                            name="membershipNumber"
+                            value={membershipNumber}
+                            onChange={e => setMembershipNumber(e.target.value)}
+                            className="w-full border border-white rounded-xl p-2 md:p-4 text-black" 
+                            placeholder="Member No." 
+                        />
+
+                        {!downloadUrl && (
+                            <button
+                                disabled={loading}
+                                className="w-full uppercase font-bold flex justify-center items-center border border-white rounded-xl p-2 md:p-4 hover:border-gooner-red hover:text-gooner-red transition-colors duration-500" 
+                                type="submit"
+                            >
+                                {loading ? 'Generating...' : 'Create'}
+                            </button>
+                        )}
+                    </div>
+
+                </form>
+
+                {downloadUrl && (
                     <a 
                         href={downloadUrl} 
                         download="membership_pass.pkpass" 
-                        className="uppercase font-bold flex justify-center items-center border border-white rounded-3xl p-2 md:p-4 hover:border-gooner-red hover:text-gooner-red min-w-[150px] md:min-w-[200px] transition-colors duration-500"
+                        className="w-full uppercase font-bold flex justify-center items-center border border-white rounded-xl p-2 md:p-4 hover:border-gooner-red hover:text-gooner-red transition-colors duration-500"
                     >
                         Download
                     </a>
