a0231d94a829a95a20e03cb3d84c44f573ae6e4a
Chris Williams <chriscancodethat@gmail.com>
2024-12-12 09:04:24 -0500

    remove glitch fx

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -14,12 +14,6 @@ export const Hero = () => {
         priority
       />
 
-      <div className="absolute z-10 left-0 right-0 top-0 bottom-0">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/background-2.jpeg')] bg-no-repeat bg-center -translate-x-[5%] animate-glitch-anim-1"></div>
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/background-2.jpeg')] bg-no-repeat bg-center -translate-x-[15%] translate-y-[3%] animate-glitch-anim-2"></div>
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/background-2.jpeg')] bg-no-repeat bg-center translate-x-[10%] animate-glitch-anim-flash"></div>
-      </div>
-
       <Scene />
 
       <div className="z-20 flex flex-col gap-4 absolute top-4 right-4 md:top-8 md:right-8 bg-black/75 p-4 rounded-lg border border-gooner-red">
